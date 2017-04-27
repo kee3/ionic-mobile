d109ba1e545b659ee8338d9490c78b4b6f4bd0c0
kee3 <keee3@yandex.ru>
2017-04-27 15:29:51 +0300

    Lab 3. Activities & UI & some features

---
+++ app/pages/page1/page1.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/page1/page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  sayHello(){
+    console.log('Hello, World!'); 
+  }
+}
